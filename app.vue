9c8ae6313b4afe00f9eb6b030923f23bdf544235
GeorgyGradinar <W2FbQC6BSedRjdc>
2023-05-31 20:36:09 +0300

    create request to server( login, logout, registration

--- app.vue
+++ app.vue
@@ -1,8 +1,4 @@
 <template>
-  <Head>
-    <Title>My App</Title>
-    <Meta name="description" http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"/>
-  </Head>
   <div>
     <NuxtLayout>
       <section class="pages">
@@ -21,11 +17,23 @@ import Footer from "~/components/FooterElement";
 import Header from "~/components/header/HeaderElement";
 import {useRoute} from "nuxt/app";
 import {onMounted, ref, watch} from "vue";
+import useHead from ".output/server/chunks/app/server.mjs";
 
 const route = useRoute();
 let isFooterVisible = ref(true);
 let isHeaderVisible = ref(true);
 
+const title = ref('My App')
+const description = ref('My App Description')
+
+useHead({
+  title,
+  meta: [{
+    name: 'description',
+    content: 'upgrade-insecure-requests',
+    'http-equiv': "Content-Security-Policy"
+  }]
+})
 
 onMounted(() => {
   checkRoute(route);
