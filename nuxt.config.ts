2c2869a25451f5a249a1124143382f52b0e899fd
danil <dyakunin@sfedu.ru>
2023-06-15 01:03:37 +0300

    prod

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,6 +8,7 @@ export default defineNuxtConfig({
     alias: {
         "@": resolve(__dirname, "/"),
     },
+    ssr: true,
     css: [
         'vuetify/lib/styles/main.sass',
         '@mdi/font/css/materialdesignicons.min.css',
