210769504d8d6c450cca1264b3a7c21633328292
GeorgyGradinar <W2FbQC6BSedRjdc>
2023-06-16 00:46:13 +0300

    add metrics script

--- mixins/seo.js
+++ mixins/seo.js
@@ -9,6 +9,9 @@ export default function seo() {
             meta: [...meta],
             link:[...links],
             script: [...scripts],
+            noscript:[{
+                children:'<div><img src="https://mc.yandex.ru/watch/93987528" style="position:absolute; left:-9999px;" alt="" /></div>'
+            }]
         })
     }
 
