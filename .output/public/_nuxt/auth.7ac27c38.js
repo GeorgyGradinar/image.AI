e03246f277d3da8a62991ff9e382da5ce8554bc3
danil <dyakunin@sfedu.ru>
2023-06-16 00:37:10 +0300

    deploy

--- .output/public/_nuxt/auth.7ac27c38.js
+++
@@ -1,1 +0,0 @@
-import{ao as o,ap as s,l as u,e as l}from"./entry.e1b30b43.js";const i=o((a,d)=>{let e=localStorage.getItem(s),t=u(e?JSON.parse(e):"");const r=l();if(a.meta.middleware&&!t.value.id)return r.push({path:"/"})});export{i as default};
--- .output/public/_nuxt/trash.43d41e3d.js
+++
@@ -1,1 +0,0 @@
-import"./entry.e1b30b43.js";const e=""+new URL("plus-project.a9183e13.svg",import.meta.url).href,_=""+new URL("trash.763712a7.svg",import.meta.url).href;export{e as _,_ as a};
