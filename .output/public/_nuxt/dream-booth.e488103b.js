c3a7900ca5ad86db869d15b7d5d66f148e8f41d3
danil <dyakunin@sfedu.ru>
2023-06-15 23:49:42 +0300

    test

---
+++ .output/public/_nuxt/dream-booth.e488103b.js
@@ -0,0 +1,1 @@
+import{a as e,b as o,o as r}from"./entry.2969b3ef.js";const t={};function c(a,n){return r(),o("p",null,"DreamBooth")}const _=e(t,[["render",c]]);export{_ as default};
---
+++ .output/public/_nuxt/guides.b01d8a2a.js
@@ -0,0 +1,1 @@
+import{a as e,b as s,o}from"./entry.2969b3ef.js";const t={name:"Guides"};function n(a,c,r,p,u,_){return o(),s("p",null,"Guides")}const i=e(t,[["render",n]]);export{i as default};
