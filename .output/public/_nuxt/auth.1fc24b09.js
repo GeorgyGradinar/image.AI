bb384044118b70e5a0d3ba9daaff5e39df0d54d6
danil <dyakunin@sfedu.ru>
2023-06-15 19:43:43 +0300

    test

---
+++ .output/public/_nuxt/auth.1fc24b09.js
@@ -0,0 +1,1 @@
+import{ao as o,ap as s,l as u,e as l}from"./entry.25c73aa7.js";const i=o((a,d)=>{let e=localStorage.getItem(s),t=u(e?JSON.parse(e):"");const r=l();if(a.meta.middleware&&!t.value.id)return r.push({path:"/"})});export{i as default};
